f17f23840b2862b97cdef2c10ec416514db37cbb
perejil117 <kevinpossos@gmail.com>
2025-09-02 17:46:07 -0500

    en progreso

--- estructuras.py
+++ estructuras.py
@@ -1,1 +1,63 @@
-print ("hola")
+def hola():
+    print ("hola ya se imprimir datos")
+
+
+#funciones 
+#diseñar una encuesta para 6 personas 
+#nombre
+#carrera
+#idea de proyecto
+
+
+
+nombres= ["wilman,kevin,stive"]
+carreras= ["ing electronica,ing electronica,ing mecatronica"]
+idea_de_proyecto= ["no sabe,sistema de gestion para una tienda,tampoco sabe"]
+def programa():
+
+    
+    nombre= input("ingresa tu nombre: ") 
+    nombres.append(nombre)
+    carrera= input("que carrera estas estudiando: ")
+    carreras.append(carrera)
+    idea_proyecto= input("que tienes pesado hacer de proyecto: ")
+    idea_de_proyecto.append(idea_proyecto)
+
+
+    print(nombres,carreras,idea_de_proyecto)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+
+    #hola()
+    programa()
+main()
